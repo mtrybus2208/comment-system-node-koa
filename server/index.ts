5f71e54733c60ed519b4153db2e2bb93285222f9
mtrybus2208 <mtrybus2208@wp.pl>
2019-10-30 22:14:15 +0100

    Added swagger config

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
 import helmet from 'koa-helmet';
+import koaSwagger from 'koa2-swagger-ui';
 import mongoose from 'mongoose';
 import { port, connexionString } from './config';
 import routing from './routes';
+import * as swaggerConfig from "./swagger.json";
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -14,11 +16,19 @@ mongoose.connect(connexionString);
 mongoose.connection.on('error', console.error);
 
 const app: Koa = new Koa();
-
+console.log(swaggerConfig);
 app
   .use(logger())
   .use(bodyParser())
-  .use(helmet());
+  .use(helmet())
+  .use(
+    koaSwagger({
+      routePrefix: '/swagger',
+      swaggerOptions: {
+        spec: swaggerConfig,
+      },
+    }),
+  );
 
 routing(app);
 
